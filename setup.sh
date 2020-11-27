d7ebcfb276201d39d341dc1f1a3ad8a1c0142c10
root <root@kali>
2020-11-27 03:52:26 -0500

    warning message in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ NC='\033[0m'
 printf "${BLUE}[*] Setting up OnBootBackup...${NC}\n"
 sudo mv backup.sh /var/backups
 sudo crontab -l | { cat; echo "@reboot sudo bash /var/backups/backup.sh"; } | sudo crontab -
-printf "${GREEN}[+] Done!${NC}\n"
+printf "${GREEN}[+] Done!\nRefer to /home/USER/.bacup.log for backup files and their timestamps${NC}\n"
+printf "${RED}[!] WARNING: Backups are deleted after 2 days! You can change this in /var/backups/backup.sh!${NC}\n"

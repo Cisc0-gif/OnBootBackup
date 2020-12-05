3292e475d4c5afcd5718b879c16e1d36fe3c3575
root <root@kali>
2020-12-05 13:48:18 -0500

    changed tar from .tgz to .tar.gz for compatability

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ datetime=$(date)
 num=$(shuf -i0-9999 -n1)
 user=USERNAMEHERE
 
-sudo tar -zcf /var/backups/$user_BU_$num.tgz /home/$user
-echo $datetime -- $user_BU_$num.tgz | sudo tee -a /home/$user/.backup.log
-sudo find /var/backups -name "$user_BU_*.tgz" -type f -mtime +2 -exec sudo shred -f {} \;
-sudo find /var/backups -name "$user_BU_*.tgz" -type f -mtime +2 -exec rm -f {} \;
+sudo tar -zcf /var/backups/$user_BU_$num.tar.gz /home/$user
+echo $datetime -- $user_BU_$num.tar.gz | sudo tee -a /home/$user/.backup.log
+sudo find /var/backups -name "$user_BU_*.tar.gz" -type f -mtime +2 -exec sudo shred -f {} \;
+sudo find /var/backups -name "$user_BU_*.tar.gz" -type f -mtime +2 -exec rm -f {} \;

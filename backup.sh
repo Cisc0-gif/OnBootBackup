7f6621d1220ba5fffe8b2c6f0973a4bdb4a2e60c
Ferenc Gozony <root@kali>
2021-02-14 19:43:19 -0500

    fixed naming mismatch in log and filename

--- backup.sh
+++ backup.sh
@@ -3,7 +3,8 @@
 datetime=$(date)
 num=$(shuf -i0-9999 -n1)
 user=USERNAMEHERE
+filename="$user_BU_$num.tar.gz"
 
-sudo tar -zcf /var/backups/$user_BU_$num.tar.gz /home/$user
-echo $datetime -- $user_BU_$num.tar.gz | sudo tee -a /home/$user/.backup.log
+sudo tar -zcf /var/backups/$filename /home/$user
+echo $datetime -- $filename | sudo tee -a /home/$user/.backup.log
 sudo find /var/backups -name "$user_BU_*" -type f -mtime +2 -delete
